70a6b8645c6386526b15fb109618e1462a76fa0f
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-11-30 19:29:00 +0100

    add paths class

---
+++ SharpMenu/Paths.cs
@@ -0,0 +1,19 @@
+﻿namespace SharpMenu
+{
+    internal static class Paths
+    {
+        internal static string AppDataRoaming;
+        internal static string LogFolder;
+        internal static string LogFile;
+
+        static Paths()
+        {
+            AppDataRoaming = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            LogFolder = Path.Combine(AppDataRoaming, "SharpHost", "logs", "SharpMenu");
+            Directory.CreateDirectory(LogFolder);
+
+            LogFile = Path.Combine(LogFolder, "SharpMenu.txt");
+        }
+    }
+}
