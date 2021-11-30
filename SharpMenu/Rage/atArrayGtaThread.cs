ba002ba9d4a3c2f574b7883e79ea9f0c5d481c26
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-11-30 16:33:31 +0100

    hardcode atArray GtaThread for now

---
+++ SharpMenu/Rage/atArrayGtaThread.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SharpMenu.Rage
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 8, Size = 0x10)]
+    internal unsafe struct atArrayGtaThread
+    {
+        internal GtaThread** data = null;
+        internal ushort size = 0;
+        internal ushort count = 0;
+    }
+}
