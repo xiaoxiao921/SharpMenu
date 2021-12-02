338c9908756245e5a8a72b406edb48322abd344f
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-12-02 14:56:39 +0100

    add Gui class

---
+++ SharpMenu/Gui.cs
@@ -0,0 +1,18 @@
+﻿using DearImguiSharp;
+
+namespace SharpMenu
+{
+    internal static class Gui
+    {
+        internal static void Draw()
+        {
+            if (ImGui.BeginMainMenuBar())
+            {
+                if (ImGui.BeginMenu("Info", true))
+                {
+
+                }
+            }
+        }
+    }
+}
