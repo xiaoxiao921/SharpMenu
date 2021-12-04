4c07e231d9c8ef8db729206711f70f7b37fd8a60
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-12-04 03:23:33 +0100

    Add Config folder and file to Paths

--- SharpMenu/Paths.cs
+++ SharpMenu/Paths.cs
@@ -3,17 +3,29 @@
     internal static class Paths
     {
         internal static string AppDataRoaming;
+        internal static string SharpHostFolder;
+
         internal static string LogFolder;
         internal static string LogFile;
+
+        internal static string ConfigFolder;
+        internal static string ConfigFile;
 
         static Paths()
         {
             AppDataRoaming = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
-            LogFolder = Path.Combine(AppDataRoaming, "SharpHost", "logs", "SharpMenu");
+            SharpHostFolder = Path.Combine(AppDataRoaming, "SharpHost");
+
+            LogFolder = Path.Combine(SharpHostFolder, "logs", "SharpMenu");
             Directory.CreateDirectory(LogFolder);
 
             LogFile = Path.Combine(LogFolder, "SharpMenu.txt");
+
+            ConfigFolder = Path.Combine(SharpHostFolder, "config", "SharpMenu");
+            Directory.CreateDirectory(ConfigFolder);
+
+            ConfigFile = Path.Combine(ConfigFolder, "SharpMenu.txt");
         }
     }
 }
