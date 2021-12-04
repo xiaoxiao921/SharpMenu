110a9200cc64022cc1696fcb6f6a9cbe69a6d19b
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-12-04 03:23:06 +0100

    Add Config system

---
+++ SharpMenu/Config.cs
@@ -0,0 +1,40 @@
+﻿namespace SharpMenu
+{
+    internal class Config
+    {
+        internal interface IValue
+        {
+            object Value { get; }
+        }
+
+        internal class Value<T> : IValue where T : unmanaged
+        {
+            internal T _Value;
+
+            object IValue.Value => _Value;
+
+            internal Value(T value)
+            {
+                _Value = value;
+                _configValues.Add(this);
+            }
+        }
+
+        internal static string FilePath;
+
+        private static List<IValue> _configValues = new();
+
+        internal static void Init(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        internal static void Save()
+        {
+            foreach (var configValue in _configValues)
+            {
+
+            }
+        }
+    }
+}
