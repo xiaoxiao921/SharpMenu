7592572b3259c532c03c8bfe97c4c9fa2a3f91b2
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-12-04 03:25:17 +0100

    ref struct for ansi string to pass to native

---
+++ SharpMenu/NativeHelpers/AnsiString.cs
@@ -0,0 +1,21 @@
+﻿namespace SharpMenu.NativeHelpers
+{
+    internal unsafe ref struct AnsiString
+    {
+        public IntPtr AllocatedString;
+
+        public AnsiString(string managedString)
+        {
+            AllocatedString = Marshal.StringToHGlobalAnsi(managedString);
+        }
+
+        public void Dispose()
+        {
+            Marshal.FreeHGlobal(AllocatedString);
+        }
+
+        public static implicit operator AnsiString(string managedString) => new AnsiString(managedString);
+        public static implicit operator IntPtr(AnsiString nativeString) => nativeString.AllocatedString;
+        public static implicit operator char*(AnsiString nativeString) => (char*)nativeString.AllocatedString;
+    }
+}
