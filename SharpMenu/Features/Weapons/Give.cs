f34e7493d1ee8433eeccc33bd70f2835f958b3ea
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-12-12 04:37:44 +0100

    give weap feature

---
+++ SharpMenu/Features/Weapons/Give.cs
@@ -0,0 +1,19 @@
+﻿using SharpMenu.Gta.Weapons;
+using SharpMenu.Rage.Natives;
+using static SharpMenu.Rage.Natives.NativeUtil;
+
+namespace SharpMenu.Features.Weapons
+{
+    internal static unsafe class Give
+    {
+        internal static Script.NoParamVoidDelegate All = All_;
+        private static void All_()
+        {
+            var playerPed = PLAYER.PLAYER_PED_ID();
+            foreach (Hash weaponHash in Enum.GetValues<WeaponHash>())
+            {
+                WEAPON.GIVE_WEAPON_TO_PED(playerPed, weaponHash, 9999, @false, @false);
+            }
+        }
+    }
+}
